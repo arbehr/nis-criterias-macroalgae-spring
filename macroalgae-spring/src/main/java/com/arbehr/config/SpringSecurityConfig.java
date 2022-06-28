258de66e8b0a159e164e40ccb2ab47ad8f9343a4
Andre Rolim Behr <arbehr@inf.ufrgs.br>
2022-06-28 15:37:10 +0000

    Correction cookie management

--- macroalgae-spring/src/main/java/com/arbehr/config/SpringSecurityConfig.java
+++ macroalgae-spring/src/main/java/com/arbehr/config/SpringSecurityConfig.java
@@ -3,7 +3,8 @@ package com.arbehr.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;  
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;  
  
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)  
@@ -11,7 +12,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")  
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .csrf().disable().antMatcher("/**")  
             .authorizeRequests()  
             .antMatchers("/", "/api/macroalgae*/**").permitAll()  
             .anyRequest().authenticated()
