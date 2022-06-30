5beaae441b9756ef9460f5f16181fcf85c3a73ee
Andre Rolim Behr <arbehr@inf.ufrgs.br>
2022-06-30 20:46:06 +0000

    Cors http22

--- macroalgae-spring/src/main/java/com/arbehr/config/SpringSecurityConfig.java
+++ macroalgae-spring/src/main/java/com/arbehr/config/SpringSecurityConfig.java
@@ -12,7 +12,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().
+        http.cors().and().csrf().
         disable()
             .authorizeRequests()
             .antMatchers(HttpMethod.OPTIONS, "/**")
